cbaa33db9c2cc0d4f4ce7c7c38000e7554d4694b
Carlos Claveria <cclaveria@siliconmedia.com>
2023-11-25 17:30:13 +0100

    Update ts-scaffold.bat

--- ts-scaffold.bat
+++ ts-scaffold.bat
@@ -1,9 +1,24 @@
+rem Silicon Media - 2023
+@echo off
+
+if "%1"=="" (
+    echo Uso: %0 [nombre_del_directorio]
+    exit /b 1
+)
+ 
+mkdir %1
+cd %1
+
+mkdir src
+echo  {"watch": ["src"],"ext":"ts","exec":"ts-node"} > nodemon.json 
+echo  console.log('Hola!');  > src\index.ts
+
 npm init -y ^
 && npm install --save-dev typescript ^
 && npm install --save-dev ts-node ^
 && npm install --save-dev @types/node ^
 && tsc --init ^
-&& sed -i 's/"scripts": {/"scripts": {  "dev": "nodemon --exec .\/node_modules\/.bin\/ts-node src\/index",/' package.json ^
-&& mkdir src ^
-&& echo   {"watch": ["src"],"ext":"ts","exec":"ts-node"} > nodemon.json ^
-&& echo console.log('Hola!');  > src\index.ts
+&& sed -i 's/"scripts": {/"scripts": {  "dev": "nodemon --exec .\/node_modules\/.bin\/ts-node src\/index",/' package.json
+
+cd ..
+
